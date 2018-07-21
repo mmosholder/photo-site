47083ac9d2271f5f59a01df1369302529d423d0b
Malinda Mosholder <m.m.mosholder@gmail.com>
2018-07-21 09:06:15 -0600

    add all the console logs

--- pages/index.vue
+++ pages/index.vue
@@ -25,18 +25,20 @@ export default {
     })
   },
 
-  // asyncData (context) {
-  //   // Check if we are in the editor mode
-  //   let version = context.query._storyblok || context.isDev ? 'draft' : 'published'
-
-  //   // Load the JSON from the API
-  //   return context.app.$storyapi.get(`cdn/stories/home`, {
-  //     version: version
-  //   }).then((res) => {
-  //     return JSON.parse(JSON.stringify(res.data));
-  //   }).catch((res) => {
-  //     context.error({ statusCode: res.response.status, message: res.response.data })
-  //   })
-  // }
+  asyncData (context) {
+    // Check if we are in the editor mode
+    let version = context.query._storyblok || context.isDev ? 'draft' : 'published'
+    console.log('async data started', version);
+    // Load the JSON from the API
+    return context.app.$storyapi.get(`cdn/stories/home`, {
+      version: version
+    }).then((res) => {
+      console.log(res);
+      return JSON.parse(JSON.stringify(res.data));
+    }).catch((res) => {
+      console.log(res);
+      context.error({ statusCode: res.response.status, message: res.response.data })
+    })
+  }
 }
 </script>
