e8854e0dc7427ba1b81bfc17177c142468b813f7
Malinda Mosholder <m.m.mosholder@gmail.com>
2018-07-21 16:30:20 -0600

    update code in for loop to be more dynamic

--- components/Category.vue
+++ components/Category.vue
@@ -6,7 +6,7 @@
           <h1 class="title-6 caps xs-mb-50 sm-mb-50 text-black grid-col-xs-6">{{blok.title}}</h1>
         </div>
         <div class="row">
-          <div class="grid-col-xs-6 xs-mb-50 md-mb-100" v-for="(item,i) in blok.images" :key="i" v-if="item.size == 'full'">
+          <div :class="[item.size == 'full' ? 'grid-col-xs-6 xs-mb-50 md-mb-100' : 'grid-col-xs-6 grid-col-sm-3 xs-mb-50 md-mb-100']" v-for="(item,i) in blok.images" :key="i">
             <div class="category-item relative">
               <img :src="item.image" alt="item.caption" class="z1">
               <div class="z2 absolute category-item-caption">
@@ -14,14 +14,14 @@
               </div>
             </div>
           </div>
-          <div class="grid-col-xs-6 grid-col-sm-3 xs-mb-50 md-mb-100" v-for="(item,i) in blok.images" :key="i" v-if="item.size == 'half'">
+          <!-- <div class="grid-col-xs-6 grid-col-sm-3 xs-mb-50 md-mb-100" v-for="(item,i) in blok.images" :key="i" v-if="item.size == 'half'">
             <div class="category-item relative">
               <img :src="item.image" alt="item.caption" class="z1">
               <div class="z2 absolute category-item-caption">
                 <p class="text-right text-16 text-white">{{item.caption}}</p>
               </div>
             </div>
-          </div>
+          </div> -->
         </div>
       </div>
     </section>
