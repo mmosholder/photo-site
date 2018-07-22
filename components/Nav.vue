e6ef5464319a06b938851f68a9163c05228a2306
Malinda Mosholder <m.m.mosholder@gmail.com>
2018-07-22 15:16:05 -0600

    update nav overlay image field

--- components/Nav.vue
+++ components/Nav.vue
@@ -12,10 +12,11 @@
         <div v-if="navOpen" class="nav-overlay">
           <div class="grid">
             <div class="row">
-              <div class="grid-col-xs-6 xs-mb-25 sm-mb-50 flex justify-space-between align-center">
+              <div class="grid-col-xs-6 xs-mb-25 sm-mb-20 flex justify-space-between align-center">
                 <h2 class="text-36 caps text-white sm-ml-20">About Mike</h2>
               </div>
-              <div class="grid-col-xs-6 grid-col-sm-4 nav-overlay-text text-18 text-white sm-ml-20" v-html="text"></div>
+              <div class="grid-col-xs-6 grid-col-sm-1 sm-ml-20"><img :src="this.$store.state.navAbout.image" alt="Mike Darnay"></div>
+              <div class="grid-col-xs-6 grid-col-sm-4 nav-overlay-text text-18 text-white" v-html="text"></div>
             </div>
           </div>
           <div class="nav-overlay-filter"></div>
