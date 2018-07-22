00756878304511cab7fbb95827c0a25b95e7ba8d
Malinda Mosholder <m.m.mosholder@gmail.com>
2018-07-22 15:53:01 -0600

    update home components

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <!-- <component v-if="story.content.component" :key="story.content._uid" :blok="story.content" :is="story.content.component"></component> -->
+    <component v-if="story.content.component" :key="story.content._uid" :blok="story.content" :is="story.content.component"></component>
   </section>
 </template>
 
