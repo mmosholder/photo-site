b99acca797f33d23463759e4647e03966d5e09ab
Malinda Mosholder <m.m.mosholder@gmail.com>
2018-07-22 21:30:30 -0600

    update sizing of images on home

--- components/HomeImageBlock.vue
+++ components/HomeImageBlock.vue
@@ -1,6 +1,6 @@
 <template>
   <a class="home-grid-item grid-span-xs-6 grid-span-sm-3 grid-span-md-2" :href="`/${info.category_link.cached_url}`">
-    <div class="home-grid-item-bg" :style="{backgroundImage: 'url(' + info.image + ')'}"></div>
+    <div class="home-grid-item-bg" :style="{backgroundImage: 'url(' + resize(info.image) + ')'}"></div>
     <div class="home-grid-item-overlay" :style="'border: 10px solid ' + info.color + ';'">
       <!-- <div class="home-grid-item-title"> -->
         <!-- <h2 class="-back title-5 text-black caps">{{info.title}}</h2> -->
@@ -13,7 +13,15 @@
 <script>
 export default {
   name: 'homeimageblock',
-  props: ['info']
+  props: ['info'],
+
+  methods: {
+    resize(image) {
+      var imageService = '//img2.storyblok.com/'
+      var path = image.replace('//a.storyblok.com', '')
+      return imageService + '405x0' + path
+    }
+  }
 }
 </script>
 
